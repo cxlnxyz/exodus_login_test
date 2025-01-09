3e43f23cb7142c908fb0bf424696f556f1d9aa7c
BEKLΛUTER <98891796+beklauter@users.noreply.github.com>
2025-01-09 11:57:00 +0100

    fixed script ig

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,7 @@ use std::process::Command;
 use std::io::{self, Write};
 
 fn main() {
-    let mut username = String::new();
-    let mut password = String::new();
     let mut list_users = String::new();
-
-    print!("Enter username: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut username).unwrap();
-    let username = username.trim();
-
-    print!("Enter password: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut password).unwrap();
-    let password = password.trim();
 
     print!("List all users? (yes/no): ");
     io::stdout().flush().unwrap();
@@ -22,10 +10,25 @@ fn main() {
     let list_users = list_users.trim().to_lowercase();
 
     let mut command = Command::new("powershell");
-    command.arg("-File").arg("check_ad_login.ps1").arg(username).arg(password);
+    command.arg("-File").arg("check_ad_login.ps1");
 
     if list_users == "yes" {
         command.arg("-listUsers");
+    } else {
+        let mut username = String::new();
+        let mut password = String::new();
+
+        print!("Enter username: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut username).unwrap();
+        let username = username.trim();
+
+        print!("Enter password: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut password).unwrap();
+        let password = password.trim();
+
+        command.arg(username).arg(password);
     }
 
     let output = command.output().expect("Failed to execute PowerShell script");
